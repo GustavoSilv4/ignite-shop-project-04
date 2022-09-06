c8806c5840f9e0e23c6b68aabb8e9818c35dbedd
Gustavo Silv4 <contato@gustavosilv4.com.br>
2022-09-06 20:37:37 -0300

    Iniciando com stitches e com SSR para o css

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,28 @@
+import { styled } from '../styles'
+
+const Button = styled('button', {
+  backgroundColor: '$rocketseat',
+  border: 0,
+  borderRadius: 4,
+  padding: '8px 8px',
+  fontSize: 30,
+  cursor: 'pointer',
+
+  span: {
+    fontWeight: 'bold',
+    marginRight: 5,
+  },
+
+  '&:hover': {
+    filter: 'brightness(0.8)',
+  },
+})
+
 export default function Home() {
-  return <h1>Hello World</h1>
+  return (
+    <Button>
+      <span>Teste</span>
+      Enviar
+    </Button>
+  )
 }
