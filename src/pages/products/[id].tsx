afd55cf61ff7300f158b428fbb75992bb03d01c3
Gustavo Silv4 <contato@gustavosilv4.com.br>
2022-09-07 11:27:22 -0300

    Criado estrutura da page Products

--- src/pages/products/[id].tsx
+++ src/pages/products/[id].tsx
@@ -1,12 +1,25 @@
 import { useRouter } from '../../../node_modules/next/router'
+import { ImageContainer, ProductContainer, ProductDetails } from '../../styles/pages/product'
 
 export default function Products() {
   const { query } = useRouter()
 
   return (
-    <div>
-      <h1>Products </h1>
-      <span>{JSON.stringify(query)}</span>
-    </div>
+    <ProductContainer>
+      <ImageContainer></ImageContainer>
+
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,90</span>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta dolorum laborum tempora, temporibus repellat
+          voluptate molestias! Rem, libero magnam, vero veniam eligendi voluptas quo suscipit nihil obcaecati sunt
+          facilis dignissimos.
+        </p>
+
+        <button>Comprar Agora</button>
+      </ProductDetails>
+    </ProductContainer>
   )
 }
