3dd002eca5787ec15713c0c39d376a051ef75827
Gustavo Silv4 <contato@gustavosilv4.com.br>
2022-09-07 09:00:19 -0300

    Criando carrossel para os produtos

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
 import Image from 'next/image'
+import { useKeenSlider } from 'keen-slider/react'
+
 import { styled } from '../styles'
 import { HomeContainer, Product } from '../styles/pages/home'
 
@@ -6,10 +8,19 @@ import camisa from '../assets/camisas/Shirt.png'
 import camisa2 from '../assets/camisas/Shirt2.png'
 import camisa3 from '../assets/camisas/Shirt3.png'
 
+import 'keen-slider/keen-slider.min.css'
+
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    },
+  })
+
   return (
-    <HomeContainer>
-      <Product>
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      <Product className="keen-slider__slide">
         <Image src={camisa} width={520} height={480} alt="" />
 
         <footer>
@@ -18,8 +29,26 @@ export default function Home() {
         </footer>
       </Product>
 
-      <Product>
+      <Product className="keen-slider__slide">
         <Image src={camisa2} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camisa X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={camisa3} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camisa X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={camisa3} width={520} height={480} alt="" />
 
         <footer>
           <strong>Camisa X</strong>
